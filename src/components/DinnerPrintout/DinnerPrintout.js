186d1bd49f57dc273621e92ab3787836ffb9f80e
Bas Straathof <bas_straathof@msn.com>
2019-02-19 14:08:14 +0100

    Refactor DinnerPrintout component

--- src/components/DinnerPrintout/DinnerPrintout.js
+++ src/components/DinnerPrintout/DinnerPrintout.js
@@ -1,40 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Sidebar from '../Sidebar/Sidebar';
 import TitleBar from '../TitleBar/TitleBar';
 
-class DinnerPrintout extends React.Component {
-  render() {
-    let printList = this.props.model
-      .getStoreData('selectedDishes')
-      .map(dish => (
-        <li class="printout-dish">
-          <img src={dish.image} alt="" />
-          <section>
-            <h2>{dish.title}</h2>
-            <p>{dish.instructions}</p>
-          </section>
-          <section>
-            <h3>Preparation</h3>
-            <p>{dish.instructions}</p>
-          </section>
-        </li>
-      ));
+const DinnerPrintout = props => {
+  let printList = props.model.getStoreData('selectedDishes').map(dish => (
+    <li class="printout-dish">
+      <img src={dish.image} alt="" />
+      <section>
+        <h2>{dish.title}</h2>
+        <p>{dish.instructions}</p>
+      </section>
+      <section>
+        <h3>Preparation</h3>
+        <p>{dish.instructions}</p>
+      </section>
+    </li>
+  ));
 
-    return (
-      <div>
-        <TitleBar model={this.props.model} />
-        <div className="dinner-printout col">
-          <ul className="printout-dishes">{printList}</ul>
-        </div>
+  return (
+    <div>
+      <TitleBar model={props.model} />
+      <div className="dinner-printout col">
+        <ul className="printout-dishes">{printList}</ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 DinnerPrintout.propTypes = {
-  model: PropTypes.object,
+  model: PropTypes.object
 };
 
 export default DinnerPrintout;

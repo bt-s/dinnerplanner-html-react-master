511336581b484127cd6b6992f8c1678a0ec70613
Bas Straathof <bas_straathof@msn.com>
2019-02-19 14:11:10 +0100

    Refactor TitleBar component

--- src/components/TitleBar/TitleBar.js
+++ src/components/TitleBar/TitleBar.js
@@ -2,29 +2,20 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {PropTypes} from 'prop-types';
 
-import Button from '../Button/Button';
-
-// function TitleBar(props) {
-class TitleBar extends React.Component {
-  render() {
-    const buttonText = 'Back to Edit Dinner';
-    return (
-      <div className="title-bar">
-        <h2>
-          My Dinner:
-          <span>{this.props.model.getStoreData('numberOfPeople')}</span>
-          people
-        </h2>
-        <Link to="/search" className="btn btn-orange btn-pointy">
-          {buttonText}
-        </Link>
-      </div>
-    );
-  }
-}
+const TitleBar = props => {
+  const numberOfPeople = props.model.getStoreData('numberOfPeople');
+  return (
+    <div className="title-bar">
+      <h2>My Dinner: {numberOfPeople} people</h2>
+      <Link to="/search" className="btn btn-orange btn-pointy">
+        Back to edit dinner
+      </Link>
+    </div>
+  );
+};
 
 TitleBar.propTypes = {
-  model: PropTypes.object,
+  model: PropTypes.object
 };
 
 export default TitleBar;
